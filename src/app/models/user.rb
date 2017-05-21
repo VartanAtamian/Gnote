d8beab2239f2d700da813cc7efe9229bcd4765b3
alice <alice@alice.com>
2017-05-21 18:01:45 +0200

    mais que c'est moche mdr

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :firstName, presence: true
+  validates :lastName, presence: true
+  validates :secretHash, presence: true, confirmation: true
 end
