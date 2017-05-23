6c532956d164190c801d4093b5a444f7e5dca99f
alice <alice@alice.com>
2017-05-23 22:05:24 +0200

    over

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,5 +1,12 @@
 class AssessmentsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @assessments = Assessment.all
+  end
+
+  def new
+    @assessment = Assessment.new
+  end
 
 end
--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,16 +1,19 @@
 class ExamsController < ApplicationController
 
-	def new
-		@exam=Exam.new
-	end
+  def index
+    @exams = Exam.all
+  end
 
-	def show
-		@exam=Exam.find(params[:id])
-	end
+  def new
+    @exam = Exam.new
+  end
+
+  def show
+    @exam = Exam.find(params[:id])
+  end
 
   def create
     @exam = Exam.new(exam_params)
-
     if @exam.save
       redirect_to @exam
     else
@@ -22,5 +25,4 @@ private
   def exam_params
     params.require(:exam).permit(:examTitle)
   end
-
 end
